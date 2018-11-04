396c630e509f7efd68ae7b3a79ebd4e18ce34801
Mark Bruckert <markbruckert1@gmail.com>
2018-11-04 17:32:22 -0500

    Added Some Presentation Stuff

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Navigation from "./Components/navigation";
 import Uber from "./uber.png";
 import Lyft from "./lyft.png";
 import Sidecar from "./sidecar.png";
+import Map from "./map.jpg";
 
 const App = () => (
 
@@ -34,6 +35,7 @@ const homeWindow = () => (
     <Link to="/food"><button class="food">🍕 Food</button></Link>
     <Link to="/dresscode"><button class="dresscode">👔 Dress Code</button></Link>
     <br/>
+    <br/>
     <hr/>
     <br/>
   </div>
@@ -49,6 +51,7 @@ const transportWindow = () => (
     <Link to="/maps"><button class="maps">🗺️ Maps</button></Link>
     <Link to="/food"><button class="food">🍕 Food</button></Link>
     <Link to="/dresscode"><button class="dresscode">👔 Dress Code</button></Link>
+    <br/>
     <br/>
     <hr/>
     <br/>
@@ -68,6 +71,7 @@ const foodWindow = () => (
     <Link to="/food"><button class="food">🍕 Food</button></Link>
     <Link to="/dresscode"><button class="dresscode">👔 Dress Code</button></Link>
     <br/>
+    <br/>
     <hr/>
     <br/>
 
@@ -86,6 +90,7 @@ const attractionWindow = () => (
     <Link to="/food"><button class="food">🍕 Food</button></Link>
     <Link to="/dresscode"><button class="dresscode">👔 Dress Code</button></Link>
     <br/>
+    <br/>
     <hr/>
     <br/>
     <h1>Attractions</h1>
@@ -102,6 +107,7 @@ const dressWindow = () => (
     <Link to="/maps"><button class="maps">🗺️ Maps</button></Link>
     <Link to="/food"><button class="food">🍕 Food</button></Link>
     <Link to="/dresscode"><button class="dresscode">👔 Dress Code</button></Link>
+    <br/>
     <br/>
     <hr/>
     <br/>
@@ -120,9 +126,28 @@ const mapsWindows = () => (
     <Link to="/food"><button class="food">🍕 Food</button></Link>
     <Link to="/dresscode"><button class="dresscode">👔 Dress Code</button></Link>
     <br/>
+    <br/>
     <hr/>
     <br/>
     <h1>Maps</h1>
+    <img class="map" src={Map}/>
+    <br/>
+    <br/>
+    <hr/>
+    <h2>The Hotel:</h2>
+    <p> This years National Convention will be held at the Gaylord Palms Resort in Washington DC. Use the following interactive map to explore the surrounding area.</p>
+    <iframe class="hotel" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3110.226301767285!2d-77.01896208465307!3d38.78144617958869!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89b7b0734d793d5d%3A0x7fccb753cbe0c4f7!2sGaylord+National+Resort+%26+Convention+Center%2C+Fort+Washington%2C+MD+20745!5e0!3m2!1sen!2sus!4v1541349162518" allowfullscreen></iframe>
+    <br/>
+    <br/>
+    <hr/>
+    <h2>The Mall Of America:</h2>
+    <p> One of the most popular attractions in Washington D.C. is the Mall of America. Click the button below to view the list of stores availble in the mall and a map of all the attractions</p>
+    <br/>
+    <img class="mall" src="https://c1.staticflickr.com/4/3089/2729609040_a222c4942d_b.jpg"/>
+    <a href="https://www.mallofamerica.com/directory" target="_blank"><button class="mallButton">View Map</button></a>
+    <br/>
+    <br/>
+    <hr/>
   </div>
 );
 
