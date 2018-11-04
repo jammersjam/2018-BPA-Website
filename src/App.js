8f6957e0382884ab07c0cdfd43564489f383a5cc
Mark Bruckert <markbruckert1@gmail.com>
2018-11-04 18:40:54 -0500

    Added Headers and Some Content

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Uber from "./uber.png";
 import Lyft from "./lyft.png";
 import Sidecar from "./sidecar.png";
 import Map from "./map.jpg";
+import { Parallax, Background } from 'react-parallax';
 
 const App = () => (
 
@@ -55,8 +56,12 @@ const transportWindow = () => (
     <br/>
     <hr/>
     <br/>
-
-    <h1>Transportation</h1>
+    <Parallax blur={10} bgImage={require('./taxi.jpg')} bgImageAlt="map" strength={200}>
+           <div class="box">
+            <h1>Transportation</h1>
+           </div>
+           <div style={{ height: '300px' }} />
+       </Parallax>
   </div>
 );
 
@@ -74,8 +79,12 @@ const foodWindow = () => (
     <br/>
     <hr/>
     <br/>
-
-    <h1>Food</h1>
+    <Parallax blur={10} bgImage={require('./pizza.jpeg')} bgImageAlt="map" strength={200}>
+           <div class="box">
+            <h1>Food</h1>
+           </div>
+           <div style={{ height: '300px' }} />
+       </Parallax>
   </div>
 );
 
@@ -93,7 +102,12 @@ const attractionWindow = () => (
     <br/>
     <hr/>
     <br/>
-    <h1>Attractions</h1>
+    <Parallax blur={10} bgImage={require('./whitehouse.JPG')} bgImageAlt="map" strength={200}>
+           <div class="box">
+            <h1>Attractions</h1>
+           </div>
+           <div style={{ height: '300px' }} />
+       </Parallax>
   </div>
 );
 
@@ -111,7 +125,12 @@ const dressWindow = () => (
     <br/>
     <hr/>
     <br/>
-    <h1>Dress Code</h1>
+    <Parallax blur={10} bgImage={require('./suit.jpg')} bgImageAlt="map" strength={200}>
+           <div class="box">
+            <h1>Dress Code</h1>
+           </div>
+           <div style={{ height: '300px' }} />
+       </Parallax>
   </div>
 );
 
@@ -129,25 +148,21 @@ const mapsWindows = () => (
     <br/>
     <hr/>
     <br/>
-    <h1>Maps</h1>
-    <img class="map" src={Map}/>
+    <Parallax blur={10} bgImage={require('./map.jpg')} bgImageAlt="map" strength={200}>
+           <div class="box">
+            <h1>Maps</h1>
+           </div>
+           <div style={{ height: '300px' }} />
+       </Parallax>
     <br/>
     <br/>
-    <hr/>
     <h2>The Hotel:</h2>
     <p> This years National Convention will be held at the Gaylord Palms Resort in Washington DC. Use the following interactive map to explore the surrounding area.</p>
     <iframe class="hotel" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3110.226301767285!2d-77.01896208465307!3d38.78144617958869!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89b7b0734d793d5d%3A0x7fccb753cbe0c4f7!2sGaylord+National+Resort+%26+Convention+Center%2C+Fort+Washington%2C+MD+20745!5e0!3m2!1sen!2sus!4v1541349162518" allowfullscreen></iframe>
     <br/>
     <br/>
     <hr/>
-    <h2>The Mall Of America:</h2>
-    <p> One of the most popular attractions in Washington D.C. is the Mall of America. Click the button below to view the list of stores availble in the mall and a map of all the attractions</p>
-    <br/>
-    <img class="mall" src="https://c1.staticflickr.com/4/3089/2729609040_a222c4942d_b.jpg"/>
-    <a href="https://www.mallofamerica.com/directory" target="_blank"><button class="mallButton">View Map</button></a>
-    <br/>
-    <br/>
-    <hr/>
+
   </div>
 );
 
