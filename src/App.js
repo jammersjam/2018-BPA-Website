94863cb208a5c3355de472fe936b357c5f836e60
Mark Bruckert <markbruckert1@gmail.com>
2018-11-05 20:16:10 -0500

    Attempt to Fix

--- src/App.js
+++ src/App.js
@@ -246,6 +246,7 @@ const attractionWindow = () => (
                         <li>Smoke or carry lighted cigarettes, cigars, or pipes into exhibit buildings, or to have a fire of any kind on the premises</li>
                         <li>Damage, deface, pick, or remove any herb, shrub, bush, tree, or turf, or portion thereof</li>
                   </ul>
+             </ul>
              <li><b><i>Natural History Museum</i></b></li>
              <ul>
                   <li><b>Address: </b>10th Street and Constitution Avenue, NW Washington, DC</li>
@@ -302,7 +303,7 @@ const attractionWindow = () => (
                   <li>10 a.m. - 5:20 p.m.</li>
                   <li><i>All bags will be checked by hand.</i></li>
              </ul>
-     </ul>
+     </li>
      <h1 align="center">Memorials/Monuments</h1>
      <h2 class="h2ul">Arlington National Cemetery</h2>
      <ul>
